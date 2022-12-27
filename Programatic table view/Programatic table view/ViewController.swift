53e8fc3bb7d9a8fcdf6be5729cec2780b94a1f36
Janvi Arora <jjanvi.arora18@gmail.com>
2022-12-27 13:05:18 +0530

    ios

---
+++ Programatic table view/Programatic table view/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Programatic table view
+//
+//  Created by Powerplay on 27/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Cell \(indexPath.row+1)"
+        return cell
+    }
+    
+}
+
